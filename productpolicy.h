8da743d96cc91be7344d3ab01c6e1210fea8456f
ecumber <ecumber05@gmail.com>
2023-03-03 20:31:23 -0800

    Add project files.

---
+++ productpolicy.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <windows.h>
+#include <winnt.h>
+
+const LPCWSTR PPRegKeyPath = L"SYSTEM\\CurrentControlSet\\Control\\ProductOptions";
+
+
+// https://www.geoffchappell.com/studies/windows/km/ntoskrnl/api/ex/slmem/productpolicy.htm
+
+struct ProductPolicyDataHeader {
+	DWORD totalsize = 0;
+	DWORD numberofvalues = 0;
+	DWORD endmarkersize = 0;
+	DWORD unknown = 0;
+	const DWORD mustbeone = 1;
+};
+
+struct ProductPolicyValueHeader {
+	WORD totalsize = 0;
+	WORD namesize = 0;
+	WORD datatype = 0;
+	WORD datasize = 0;
+	DWORD flags = 0;
+	DWORD unknown = 0;
+};
+
+enum ProductPolicyValueType {
+	PP_NONE = 0,
+	PP_SZ = 1,
+	PP_BINARY = 3,
+	PP_DWORD = 4,
+	PP_MULTI_SZ = 7,
+};
+
+struct ProductPolicyValue {
+	ProductPolicyValueHeader header;
+	WCHAR* policyname;
+	ProductPolicyValueType valuetype;
+};
+
+struct ProductPolicyBlob {
+	ProductPolicyDataHeader* dataheader;
+	ProductPolicyValue* value;
+};
